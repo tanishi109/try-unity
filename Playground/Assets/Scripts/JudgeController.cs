8d23b03f4b116c14bcb32258463d5198867c4002
tanishi109 <note109@gmail.com>
2017-12-30 22:52:27 +0900

    get variable from playerGrower

---
+++ Playground/Assets/Scripts/JudgeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JudgeController : MonoBehaviour {
+	private GameObject center;
+
+	// Use this for initialization
+	void Start () {
+		center = GameObject.Find ("/Player/Center");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		PlayerGrower playerGrower = center.GetComponent<PlayerGrower>();
+		print (playerGrower.key);
+	}
+}
