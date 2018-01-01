2f38e19b232c1e2b7833a0d5f9f16aa6835ed43c
tanishi109 <note109@gmail.com>
2018-01-01 14:34:16 +0900

    judge battle

--- Playground/Assets/Scripts/JudgeController.cs
+++ Playground/Assets/Scripts/JudgeController.cs
@@ -4,23 +4,77 @@ using UnityEngine;
 
 public class JudgeController : MonoBehaviour {
 	private GameObject[] fingers1;
+	private GameObject[] fingers2;
+	private GameObject player1;
+	private GameObject player2;
 
-	// Use this for initialization
-	void Start () {
+	private void OnEnable()
+	{
 		fingers1 = new GameObject[] {
 			GameObject.Find ("/Player/Left"),
 			GameObject.Find ("/Player/Center"),
 			GameObject.Find ("/Player/Right")
 		};
+		fingers2 = new GameObject[] {
+			GameObject.Find ("/Player2/Left"),
+			GameObject.Find ("/Player2/Center"),
+			GameObject.Find ("/Player2/Right")
+		};
+		player1 = GameObject.Find ("/Player");
+		player2 = GameObject.Find ("/Player2");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		foreach (GameObject f in fingers1) {
-			PlayerGrower playerGrower = f.GetComponent<PlayerGrower>();
-			print ("");
-			print (playerGrower.key);
-			print (playerGrower.length);
+
+	private void Update()
+	{
+		int winner = GetWinner();
+
+		if (winner == 1) {
+			PlayerHealth ph = player2.GetComponent<PlayerHealth> ();
+			ph.TakeDamage ();
 		}
+		if (winner == 2) {
+			PlayerHealth ph = player1.GetComponent<PlayerHealth> ();
+			ph.TakeDamage ();
+		}
+	}
+
+	private int GetWinner()
+	{
+		int weapon1 = GetWeapon (fingers1);
+		int weapon2 = GetWeapon (fingers2);
+
+		if (weapon1 == weapon2) {
+			return 0; // drawn game
+		}
+
+		bool is2Win = (weapon1 - weapon2 + 3) % 3 == 1;
+
+		if (is2Win) {
+			return 2;
+		}
+
+		return 1;
+	}
+
+	private int GetWeapon(GameObject[] fingers)
+	{
+		int fcount = 0;
+		foreach (GameObject f in fingers) {
+			PlayerGrower pg = f.GetComponent<PlayerGrower> ();
+			if (pg.length >= 30f) {
+				fcount += 1;
+			}
+		}
+		if (fcount == 0) {
+			return 0; // Rock
+		}
+		if (fcount == 2) {
+			return 1; // Scissors
+		}
+		if (fcount == 3) {
+			return 2; // Paper
+		}
+
+		return 0;
 	}
 }
--- Playground/Assets/Scripts/PlayerHealth.cs
+++ Playground/Assets/Scripts/PlayerHealth.cs
@@ -5,55 +5,45 @@ using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour {
 
-	public float m_StartingHealth = 100f;               // 開始時の各タンクの体力の値
-	public Slider m_Slider;                             // 現在のタンクの体力を示すスライダー
-	public Image m_FillImage;                           // スライダーの Image コンポーネント
-	public Color m_FullHealthColor = Color.green;       // 体力が満タンのときの体力バーの色
-	public Color m_ZeroHealthColor = Color.red;         // 体力が 0 になったときの体力バーの色
-	private float m_CurrentHealth;                      // 現在のタンクの体力値
-	private bool m_Dead;                                // タンクの体力値が 0 を下回ったかどうか
+	public float m_StartingHealth = 100f;
+	public Slider m_Slider;
+	public Image m_FillImage;
+	public Color m_FullHealthColor = Color.green;
+	public Color m_ZeroHealthColor = Color.red;
+	private float m_CurrentHealth;
+	private bool m_Dead;
 
 	private void OnEnable()
 	{
-		// タンクが有効にされるとき、タンクの体力をリセットし、倒されていない状態にリセットします。
 		m_CurrentHealth = m_StartingHealth;
 		m_Dead = false;
 
-		// 体力スライダーの値と色を更新
 		SetHealthUI();
 	}
 
-	public void TakeDamage (float amount)
+	public void TakeDamage ()
 	{
-		// 受けたダメージに基づいて現在の体力を削減
-		m_CurrentHealth -= amount;
+		m_CurrentHealth -= 0.4f;
 
-		// 適切な UI 要素に変更
 		SetHealthUI ();
 
-		// 現在の体力が 0 を下回り、かつ、まだ登録されていなければ、 OnDeath を呼び出します。
-		if (m_CurrentHealth <= 0f && !m_Dead)
-		{
+		if (m_CurrentHealth <= 0f && !m_Dead) {
 			OnDeath ();
 		}
 	}
 
 	private void SetHealthUI ()
 	{
-		// スライダーに適切な値を設定
 		m_Slider.value = m_CurrentHealth;
 
-		// 開始時に対する現在の体力のパーセントに基づいて、選択した色でバーを満たします。
 		m_FillImage.color = Color.Lerp (m_ZeroHealthColor, m_FullHealthColor, m_CurrentHealth / m_StartingHealth);
 	}
 
 
 	private void OnDeath ()
 	{
-		// フラグを設定して、この関数が1 度しか呼び出されないようにします。
 		m_Dead = true;
 
-		// タンクをオフにします。
 		gameObject.SetActive (false);
 	}
 }
